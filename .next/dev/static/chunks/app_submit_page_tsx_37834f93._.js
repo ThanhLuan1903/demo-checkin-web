d2225567386ee98a433628ac45c8ed9545c573e2
luan-tran1903 <luan.tran@techsoft.one>
2025-12-08 17:23:18 +0700

    init project

--- .next/dev/static/chunks/app_submit_page_tsx_37834f93._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_32f4c8e0._.js",
-  "static/chunks/_449e7693._.js"
-],
-    source: "dynamic"
-});
--- .next/server/app/api/checkin/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/checkin/route.js")
-R.c("server/chunks/[root-of-the-server]__03fe02e0._.js")
-R.c("server/chunks/[root-of-the-server]__557ee8cd._.js")
-R.c("server/chunks/_next-internal_server_app_api_checkin_route_actions_b98240ac.js")
-R.m(3326)
-module.exports=R.m(3326).exports
